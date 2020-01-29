87cacacc7e91edd17c80b56ff2f5501c7c773375
Levi Durfee <levi.durfee@gmail.com>
2020-01-29 10:13:12 -0500

    v3

--- functions.php
+++ functions.php
@@ -1,15 +1,13 @@
 <?php
 
-namespace Bulldog;
-
-if(!function_exists(__NAMESPACE__ . 'dd')) {
+if(!function_exists('Dd')) {
     function dd($input) {
-        dump($input);
+        Dump($input);
         die();
     }
 }
 
-if(!function_exists(__NAMESPACE__ . 'dump')) {
+if(!function_exists('Dump')) {
     function dump($input) {
         if(BD_WEB_REQUEST) {
             echo '<pre>';
@@ -23,13 +21,13 @@ if(!function_exists(__NAMESPACE__ . 'dump')) {
     }
 }
 
-if(!function_exists(__NAMESPACE__ . 'WriteLine')) {
+if(!function_exists('WriteLine')) {
     function WriteLine($input) {
         echo $input . "\n";
     }
 }
 
-if(!function_exists(__NAMESPACE__ . 'PrintCode')) {
+if(!function_exists('PrintCode')) {
     function PrintCode($input, $label = '') {
         if($label) {
             echo '<h2>'.$label.'</h2>';
@@ -40,7 +38,7 @@ if(!function_exists(__NAMESPACE__ . 'PrintCode')) {
     }
 }
 
-if(!function_exists(__NAMESPACE__ . 'Slugify')) {
+if(!function_exists('Slugify')) {
     function Slugify($str) {
         $search = array('Ș', 'Ț', 'ş', 'ţ', 'Ş', 'Ţ', 'ș', 'ț', 'î', 'â', 'ă', 'Î', 'Â', 'Ă', 'ë', 'Ë');
         $replace = array('s', 't', 's', 't', 's', 't', 's', 't', 'i', 'a', 'a', 'i', 'a', 'a', 'e', 'E');
@@ -52,7 +50,7 @@ if(!function_exists(__NAMESPACE__ . 'Slugify')) {
     }
 }
 
-if(!function_exists(__NAMESPACE__ . 'ShortHash')) {
+if(!function_exists('ShortHash')) {
     function ShortHash($input, $len = 10) {
         return substr(hash('sha256', $input), 0, $len);
     }
